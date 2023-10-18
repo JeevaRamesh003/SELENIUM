ecde667c91db2de536a1d430ff24a1b3b9aee615
JeevaRamesh003 <134124575+JeevaRamesh003@users.noreply.github.com>
2023-10-18 22:32:57 +0530

    Update demo.py

--- pythonProject/class/demo.py
+++ pythonProject/class/demo.py
@@ -1,14 +1,1 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import service
 
-driver= webdriver.Chrome("C:\chromedriver.exe.exe")
-
-driver.get("https://www.youtube.com/")
-print(driver.title)
-"""""  def test_signup_linkvisible(self, driver):
-      self.init_driver = driver
-      time.sleep(10)
-      driver.implicitly_wait(10)
-      flag = self.is_signup_link_exist()
-      assert flag"""""
